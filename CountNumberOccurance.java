b7b7c6f80820183da1c5aeb07d4ad55ab3da8d69
NandhiniKR <krinivs@gmail.com>
2016-06-03 22:21:17 +0000

    Add files via upload

---
+++ CountNumberOccurance.java
@@ -0,0 +1,35 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class CountNumberOccurance {
+
+	
+
+	private static Scanner input;
+
+	public static void main(String[] args) {
+		
+		 input = new Scanner(System.in);
+
+	        int[] number = new int[100];
+
+	       
+	        System.out.print("Enter the Nos. (Enter 0 to end the Input) : ");
+	        for (int i = 0; i < number.length; i++) {
+	            int a = input.nextInt();
+	            number[a] += a;
+	            if (a == 0)
+	                break;
+	        }
+	        for (int i = 1; i < number.length; i++) {
+	            if (number[i] != 0) {
+	                if (number[i] / i > 1)
+	                    System.out.println(i + " occured " + number[i] / i + " times");
+	                else
+	                    System.out.println(i + " occured " + number[i] / i + " time");               }
+	        }
+
+	}
+
+}
